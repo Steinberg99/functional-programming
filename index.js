59553913fa78b7dd5c8645b42005ef4181e5e375
Steinberg99 <stein.bergervoet@gmail.com>
2021-10-29 12:33:34 +0200

    Refacor index.js, home.pug & styles.css

--- index.js
+++ index.js
@@ -1,151 +1,72 @@
 const express = require('express');
 const app = express();
 const port = 4200;
+const data = require('./data/data.json');
+const {
+  commaSeperatedStringToArray,
+  hexColor,
+  hexColorArray,
+  distribution,
+  percentages,
+  sortObject,
+  sortedObjectToArray,
+  gradient
+} = require('./modules/functions.js');
 
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }));
 
-const data = require('./data.json');
-const hexColors = require('./hexColors.json');
+let colorArrays = [];
+let hexColorArrays = [];
 
-let clothingColors = [];
-let mergedClothingColors = [];
-let clothingHexColors = [];
+let gradients = [];
+
 let colorDistribution;
 let colorPercentages;
+
 let sortedColorDistribution;
 let sortedColorPercentages;
-let sortedClothingHexColors = [];
-let colorGradients = [];
+
+let sortedColors;
+let sortedHexColors;
+
+function main() {
+  // Create the color arrays and turn them into hex color arrays.
+  for (let i = 0; i < data.length; i++) {
+    colorArrays.push(commaSeperatedStringToArray(data[i]['clothing-colors']));
+  }
+  hexColorArrays = colorArrays.map(array => hexColorArray(array));
+
+  // Create the CSS gradients based on the hex color arrays.
+  for (let i = 0; i < hexColorArrays.length; i++) {
+    gradients.push(gradient(hexColorArrays[i]));
+  }
+
+  // Create the color distribution and the color percentages.
+  colorDistribution = distribution([].concat.apply([], colorArrays));
+  colorPercentages = percentages(colorDistribution);
+
+  // Sort the color distribution and color percentages objects.
+  sortedColorDistribution = sortObject(colorDistribution);
+  sortedColorPercentages = sortObject(colorPercentages);
+
+  // Turn the sorted color distribution object into an array of hex colors.
+  sortedColors = sortedObjectToArray(sortObject(colorDistribution));
+  sortedHexColors = sortedColors.map(color => hexColor(color));
+  console.log(sortedHexColors);
+}
+
+main();
 
 // Render the homepage.
 app.get('/', (req, res) => {
   res.render('home', {
-    colorGradients: colorGradients,
-    sortedClothingHexColors: sortedClothingHexColors,
+    gradients: gradients,
+    sortedHexColors: sortedHexColors,
     sortedColorPercentages: sortedColorPercentages,
-    getHexColor: getHexColor
+    hexColor: hexColor
   });
 });
-
-// Main function.
-function main() {
-  // Retrieve the clothing colors from the data.json file.
-  for (let i = 0; i < data.length; i++) {
-    clothingColors.push(
-      commaSeperatedStringToArray(data[i]['clothing-colors'])
-    );
-  }
-  // Merge the cloting colors array.
-  mergedClothingColors = [].concat.apply([], clothingColors);
-
-  // Retrieve the hex colors codes.
-  clothingHexColors = clothingColors.map(getHexColors);
-
-  // Retrieve the distribution of colors.
-  colorDistribution = distribution(mergedClothingColors);
-
-  // Retrieve the color percentages based on the distribution of colors.
-  colorPercentages = percentage(colorDistribution);
-
-  // Sort the color distribution object.
-  sortedColorDistribution = sortObject(colorDistribution);
-
-  console.log(sortedColorDistribution);
-
-  // Sort the color percentages object.
-  sortedColorPercentages = sortObject(colorPercentages);
-
-  Object.keys(sortedColorDistribution).forEach(key => {
-    for (let i = 0; i < sortedColorDistribution[key]; i++) {
-      sortedClothingHexColors.push(getHexColor(key));
-    }
-  });
-
-  // Retrieve the css gradient strings.
-  for (let i = 0; i < clothingHexColors.length; i++) {
-    colorGradients.push(gradient(clothingHexColors[i]));
-  }
-}
-
-function commaSeperatedStringToArray(string) {
-  // Replace whitespace using Regex.
-  string = string.replace(/\s+/g, '');
-  return string.split(',');
-}
-
-// Retrieves the hex colors.
-function getHexColors(colors) {
-  return colors.map(color => hexColors[color]);
-}
-
-function getHexColor(color) {
-  return hexColors[color];
-}
-
-// Retrieves the distribution of strings and numbers in an array.
-function distribution(data) {
-  let object = {};
-  for (let i = 0; i < data.length; i++) {
-    if (!object.hasOwnProperty(data[i])) {
-      Object.assign(object, { [data[i]]: 0 });
-    }
-    object[[data[i]]]++;
-  }
-  return object;
-}
-
-// Retrieves the distribution of strings and numbers in an array in percentages.
-function percentage(object) {
-  let totalAnswers = 0;
-  // Retrieve the total answers.
-  Object.keys(object).forEach(key => {
-    totalAnswers += object[key];
-  });
-  Object.keys(object).forEach(key => {
-    object[key] = Math.round((object[key] / totalAnswers) * 100);
-  });
-  return object;
-}
-
-// Retrieves the CSS gradient based on the provided hex color codes.
-function gradient(colors) {
-  if (colors[1]) {
-    let gradient = 'linear-gradient(to right,';
-    for (let i = 0; i < colors.length - 1; i++) {
-      // To create the desired effect the color needs to be repeated twice.
-      gradient += ` ${colors[i]} ${(100 / (colors.length - 1)) * i}%,`;
-      gradient += ` ${colors[i]} ${(100 / (colors.length - 1)) * (i + 1)}%,`;
-    }
-    gradient += ` ${colors[colors.length - 1]} ${
-      (100 / (colors.length - 1)) * (colors.length - 2)
-    }%,`;
-    gradient += ` ${colors[colors.length - 1]} ${
-      (100 / (colors.length - 1)) * (colors.length - 1)
-    }%)`;
-    return gradient;
-  }
-  // If the array of colors only contains one color this color gets returned.
-  return colors[0];
-}
-
-// Sort object based on numeric values.
-function sortObject(object) {
-  var array = [];
-  for (let key in object) {
-    array.push([key, object[key]]);
-  }
-  array.sort(function (a, b) {
-    return b[1] - a[1];
-  });
-  let sortedObject = {};
-  array.forEach(item => {
-    sortedObject[item[0]] = item[1];
-  });
-  return sortedObject;
-}
-
-main();
 
 // Start server on local port.
 app.listen(port, () => {
---
+++ modules/functions.js
@@ -0,0 +1,100 @@
+const hexColorData = require('../data/hexColorData.json');
+
+// Seperates a comma separated string into an array of strings.
+function commaSeperatedStringToArray(string) {
+  // Replace whitespace using Regex.
+  string = string.replace(/\s+/g, '');
+  return string.split(',');
+}
+
+// Turns a string into a hex color.
+function hexColor(string) {
+  return hexColorData[string];
+}
+
+// Turns an array of strings into hex colors.
+function hexColorArray(array) {
+  return array.map(string => hexColorData[string]);
+}
+
+// Retrieves the distribution of values in an array.
+function distribution(array) {
+  let object = {};
+  for (let i = 0; i < array.length; i++) {
+    if (object.hasOwnProperty(array[i])) {
+      object[[array[i]]]++;
+    } else {
+      Object.assign(object, { [array[i]]: 1 });
+    }
+  }
+  return object;
+}
+
+// Retrieves the distribution of values in an array in percentages.
+function percentages(object) {
+  let totalAnswers = 0;
+  // Retrieve the total answers.
+  Object.keys(object).forEach(key => {
+    totalAnswers += object[key];
+  });
+  Object.keys(object).forEach(key => {
+    object[key] = Math.round((object[key] / totalAnswers) * 100);
+  });
+  return object;
+}
+
+// Sort the keys of an object based on their numeric values.
+function sortObject(object) {
+  var array = [];
+  for (let key in object) {
+    array.push([key, object[key]]);
+  }
+  array.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+  let sortedObject = {};
+  array.forEach(item => {
+    sortedObject[item[0]] = item[1];
+  });
+  return sortedObject;
+}
+
+// Turns a sorted object into an array.
+function sortedObjectToArray(object) {
+  let array = [];
+  Object.keys(object).forEach(key => {
+    for (let i = 0; i < object[key]; i++) {
+      array.push(key);
+    }
+  });
+  return array;
+}
+
+// Creates a CSS gradient based on an array of hex colors.
+function gradient(colorArray) {
+  let interval = 100 / (colorArray.length - 1);
+  let string = 'linear-gradient(to right,';
+  // If the array contains more then one color, create the gradient.
+  if (colorArray[1]) {
+    for (let i = 0; i < colorArray.length; i++) {
+      // To create the desired effect the hex color has to be repeated.
+      string += ` ${colorArray[i]} ${interval * i}%,`;
+      string += ` ${colorArray[i]} ${interval * (i + 1)}%,`;
+    }
+    string = string.slice(0, -1) + ')';
+    return string;
+  }
+  // If the array of colors only contains one color return this color.
+  return colorArray[0];
+}
+
+module.exports = {
+  commaSeperatedStringToArray: commaSeperatedStringToArray,
+  hexColor: hexColor,
+  hexColorArray: hexColorArray,
+  distribution: distribution,
+  percentages: percentages,
+  sortObject: sortObject,
+  sortedObjectToArray: sortedObjectToArray,
+  gradient: gradient
+};
